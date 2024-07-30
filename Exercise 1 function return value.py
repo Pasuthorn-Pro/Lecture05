94e219f97c7edc9b125facca9e82fc7aed0a9063
Pasuuu-Yahoo <pasuthorn1406@gmail.com>
2024-07-30 19:01:12 +0700

    Lecture05

---
+++ Exercise 1 function return value.py
@@ -0,0 +1,19 @@
+def is_armstrong(number):
+    number_str = str(number)
+
+    num_digits = len(number_str)
+
+    total = 0
+
+    for digit in number_str:
+        total += int(digit) ** num_digits
+
+    if total == number:
+        output = " + ".join(f"{int(digit)}^{num_digits}" for digit in number_str)
+        #print(True)
+        print(f"{number} = {output}")
+        return True
+    else:
+        return False
+
+print(is_armstrong(371))
