3aac00f4ee62051360ab88d2e9903196fcd4e64c
Pasuuu-Yahoo <pasuthorn1406@gmail.com>
2024-07-31 01:28:40 +0700

    Lecture05

--- Operation/main.py
+++ Operation/main.py
@@ -1,13 +1,16 @@
-#main.py
+# main.py
+
+import sys
+sys.path.append('.')
 
 import math_operations
 
-result_add = math_operations.add(10,5)
-result_subtract = math_operations.subtract(10,5)
-result_multiply = math_operations.multiply(10,5)
-result_divide = math_operations.divide(10,5)
+result_add = math_operations.add(10, 5)
+result_subtract = math_operations.subtract(10, 5)
+result_multiply = math_operations.multiply(10, 5)
+result_divide = math_operations.divide(10, 5)
 
-print(f"Addtion: {result_add}")
+print(f"Addition: {result_add}")
 print(f"Subtraction: {result_subtract}")
 print(f"Multiplication: {result_multiply}")
 print(f"Division: {result_divide}")
