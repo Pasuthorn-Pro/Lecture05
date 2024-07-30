a1824134caeba151e544b59650615ac7949391a1
Pasuuu-Yahoo <pasuthorn1406@gmail.com>
2024-07-30 19:14:20 +0700

    Lecture05

---
+++ function scope and variables.py
@@ -0,0 +1,5 @@
+def my_function():
+    local_variables = "I'm inside the function"
+    print(local_variables)
+
+my_function()
---
+++ function scope and variables002.py
@@ -0,0 +1,8 @@
+global_variables = "I'm outside the function"
+
+def my_function():
+    print(global_variables)
+
+my_function()
+
+print(global_variables)
