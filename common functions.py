b3e008bc565ba0631ebcceec79a51f930fe19cb7
Pasuuu-Yahoo <pasuthorn1406@gmail.com>
2024-07-31 02:22:55 +0700

    Lecture05

---
+++ common functions.py
@@ -0,0 +1,19 @@
+a = abs(-4/3)
+b = round(-4/3)
+c = min(3,2,5)
+d = min("c","a","b")
+e = max(3,2,5)
+f = [3,2,1]
+sorted(f)
+g = [4,5,6]
+sum(g)
+h = [4/3, 2/3, 1/3, 1/3, 1/3]
+sum(h)
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(sum(g))
+print(sum(h))
